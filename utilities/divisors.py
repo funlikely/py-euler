697f2c0d8a5455de64bdbce7b592541f7295cdfc
oldflat <smJU.20tackom$eat>
2023-02-18 10:38:45 -0500

    euler 027 WIP, refactoring more functions into primes.py III

--- utilities/divisors.py
+++ utilities/divisors.py
@@ -1,4 +1,8 @@
 # Divisors utilities
+import math
+
+from utilities.primes import prime_factors
+
 
 def divisor_counter(a):
     if a < 0:
@@ -10,3 +14,18 @@ def divisor_counter(a):
         if a % i == 0:
             count += 1
     return count + 1
+
+
+def get_divisors(n):
+    prime_factor_list = prime_factors(n)
+    divisor_list = [1]
+    for i in range(len(prime_factor_list[1])):
+        new_divisor_list = []
+        for j in range(prime_factor_list[1][i] + 1):
+            for k in range(len(divisor_list)):
+                if j > 0:
+                    new_divisor_list.append(int(math.pow(prime_factor_list[0][i], j)) * divisor_list[k])
+        divisor_list += new_divisor_list
+    divisor_list = list(dict.fromkeys(divisor_list))
+    divisor_list.sort()
+    return divisor_list[:(len(divisor_list) - 1)]
