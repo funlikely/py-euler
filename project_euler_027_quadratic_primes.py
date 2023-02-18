1305b23f18fd3cc7acd8670565e7844dade3a3b5
oldflat <smJU.20tackom$eat>
2023-02-18 10:47:55 -0500

    euler 027 complete

--- project_euler_027_quadratic_primes.py
+++ project_euler_027_quadratic_primes.py
@@ -37,7 +37,10 @@ def prime_generator(n, a, b):
     return n * n + a * n + b
 
 
-def main():
+def test_on_original_prime_quadratic(tests_and_logs):
+    if not tests_and_logs:
+        return
+
     prime_sieve_027 = prime_sieve(8300)
 
     print(f"the first and last couple elements of the prime sieve are {prime_sieve_027[:5]} and {prime_sieve_027[-5:]}")
@@ -52,12 +55,57 @@ def main():
         prime_count += 1
 
         if prime_count % 10 == 0:
-            print(f"{prime_count}th prime is {index}")
+            print(f"{prime_count}th generated prime is {index}")
 
         n += 1
         index = prime_generator(n, a, b)
 
     print(f"The number of consecutive primes generated for a = {a} and b = {b} is {prime_count}")
+
+
+def get_prime_count_on_quadratic(a, b, prime_sieve):
+    n = 1
+    prime_count = 0
+    index = prime_generator(n, a, b)
+
+    while prime_sieve[index]:
+        prime_count += 1
+
+        if prime_count % 10 == 0:
+            print(f"{prime_count}th generated prime is {index}")
+
+        n += 1
+        index = prime_generator(n, a, b)
+
+    return prime_count
+
+
+def find_best_prime_quadratic(tests_and_logs):
+    prime_sieve_027 = prime_sieve(8300)
+    best_a = 1
+    best_b = 1
+    best_prime_count = 1
+
+    for a in range(-1000, 1000):
+        for b in range(-1000, 1000):
+            prime_count = get_prime_count_on_quadratic(a, b, prime_sieve_027)
+            if prime_count > best_prime_count:
+                if prime_count > best_prime_count + 30 and tests_and_logs:
+                    print(f"latest great updated quadratic, n*n + {a}*n + {b}, with {prime_count} primes")
+                best_a = a
+                best_b = b
+                best_prime_count = prime_count
+
+    return best_a, best_b, best_prime_count
+
+
+def main():
+    tests_and_logs = False
+
+    test_on_original_prime_quadratic(tests_and_logs)
+    a, b, prime_count = find_best_prime_quadratic(tests_and_logs)
+    print(f"The best quadratic is n*n + {a}*n + {b}, with {prime_count} primes.")
+    print(f"The Answer to Euler 027 is a * b = {a * b}.")
 
 
 if __name__ == "__main__":
