eb03b36feb940bee392ddd3686646d15cb2fe792
oldflat <smJU.20tackom$eat>
2023-02-18 10:27:31 -0500

    euler 027 WIP, refactoring more functions into primes.py

--- project_euler_010_summation_of_primes.py
+++ project_euler_010_summation_of_primes.py
@@ -5,37 +5,20 @@
 # The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 #
 # Find the sum of all the primes below two million.
-
-def primeSieveUpTo(n):
-    sieve = [True] * n
-    sieve[0] = sieve[1] = False
-    primeList = []
-    primesFound = 0
-    sieveCounter = 2
-    while sieveCounter < len(sieve):
-        if sieve[sieveCounter]:
-            primesFound = primesFound + 1
-            primeList.append(sieveCounter)
-            sieveActionCounter = sieveCounter*2
-            while sieveActionCounter < len(sieve):
-                sieve[sieveActionCounter] = False
-                sieveActionCounter += sieveCounter
-        sieveCounter += 1
-    for sieveCounter in range(sieveCounter, len(sieve)):
-        sieve[sieveCounter] = False
-
-    print(str(primesFound) + ' primes found!')
-    return sieve
+from utilities.primes import prime_sieve_up_to
 
 
-numPrimes = 2000000
-primes = primeSieveUpTo(numPrimes)
-primesOrdinal = 1
-primesSum = 0
-for i in range(2, len(primes)):
-    if primes[i]:
-        primesSum += i
+def main():
+    num_primes = 2000000
+    primes = prime_sieve_up_to(num_primes)
+    primes_sum = 0
+    for i in range(2, len(primes)):
+        if primes[i]:
+            primes_sum += i
 
-print("The sum of the primes less than " + str(numPrimes) + " is " + str(primesSum))
+    print("The sum of the primes less than " + str(num_primes) + " is " + str(primes_sum))
 
+
+if __name__ == "__main__":
+    main()
 
