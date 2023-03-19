804af7bea0171e4026488f38527819a0f3058614
oldflat <smJU.20tackom$eat>
2023-03-19 11:12:02 -0400

    euler 029 nonfunctioning changes

---
+++ utilities/prime_decomposition.py
@@ -0,0 +1,15 @@
+"""
+    Prime decomposition
+    prime_decomposition of 1320 is 2^3 + 3^1 + 5^1 + 7^0 + 11^1, or [3, 1, 1, 0, 1]
+"""
+
+
+def get_pd(value):
+    pass
+
+
+class PrimeDecomposition:
+    
+    def __init__(self, value):
+        self.value = value
+        self.pd = get_pd(value)
