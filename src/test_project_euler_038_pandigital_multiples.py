e57d6b5a64fe84c8efa6546ef7d8b3f79a5d9882
oldflat <smJU.20tackom$eat>
2023-03-26 15:34:50 -0400

    Added an initial unit test, and some refactoring

---
+++ src/test_project_euler_038_pandigital_multiples.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+
+from project_euler_038_pandigital_multiples import PandigitalNumbers
+
+
+class TestPandigitalNumbers(TestCase):
+
+    def test_get_pandigital_numbers2(self):
+        p = PandigitalNumbers()
+        assert max(p.get_pandigital_numbers2()) == "932718654"
+
+    def test_get_pandigital_numbers3(self):
+        p = PandigitalNumbers()
+        assert max(p.get_pandigital_numbers3()) == "932718654"
