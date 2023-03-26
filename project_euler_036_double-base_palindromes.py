8646b769b756c7bbf99948f72f1b5d8a5449d5d7
oldflat <smJU.20tackom$eat>
2023-03-26 01:14:57 -0400

    euler 036 solution

--- project_euler_036_double-base_palindromes.py
+++ project_euler_036_double-base_palindromes.py
@@ -13,15 +13,40 @@
 """
 
 
+def is_palindrome(param):
+    return param == param[::-1]
+
+
+def binary_str(n):
+    result = ""
+    while n > 1:
+        if n % 2 == 1:
+            result = "1" + result
+        else:
+            result = "0" + result
+        n = int(n/2)
+    if n % 2 == 1:
+        result = "1" + result
+    else:
+        result = "0" + result
+    return result
+
+
 def get_doubledromes():
-    pass
+    result = []
+
+    for i in range(1000000):
+        if is_palindrome(str(i)) and is_palindrome(binary_str(i)):
+            result.append(i)
+
+    return result
 
 
 def main():
     answer = get_doubledromes()
     print(f"The Answer to Project Euler 036 is {sum(answer)}")
 
-    # The Answer to Project Euler 036
+    # The Answer to Project Euler 036 is 872187
 
 
 if __name__ == "__main__":
