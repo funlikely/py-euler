438c9a3fed5dc067737ccf2ba9962152e8684f21
oldflat <smJU.20tackom$eat>
2023-03-25 21:49:15 -0400

    euler 031 solution, but dirty

--- project_euler_031_coin_sum.py
+++ project_euler_031_coin_sum.py
@@ -73,29 +73,40 @@ def get_num_coin_sum():
 
 
 def get_coin_sums_iterative(target, denominations):
-    ways = []
-    way = []
-    for i in range(len(denominations)):
-        sum = 0
-        # while sum < target:
-    return []
+    count = 0
+    for a in range(0, target + 1, denominations[0]):
+        for b in range(0, target + 1, denominations[1]):
+            for c in range(0, target + 1, denominations[2]):
+                for d in range(0, target + 1, denominations[3]):
+                    for e in range(0, target + 1, denominations[4]):
+                        for f in range(0, target + 1, denominations[5]):
+                            for g in range(0, target + 1, denominations[6]):
+                                # for h in range(0, target + 1, denominations[7]):
+                                # if a + b + c + d + e + f + g + h == target:
+                                if a + b + c + d + e + f + g <= target:
+                                    h = target - (a + b + c + d + e + f + g)
+                                    if count % 10000 == 0:
+                                        print(f"count = {count}, a={a}, b={b}, c={c}, d={d}, e={e}, f={f}, g={g}, h={h}")
+                                    count += 1
+    return count
 
 
 def get_num_coin_sum_iterative():
     denominations = [200, 100, 50, 20, 10, 5, 2, 1]
-    denominations = [10, 5, 2, 1]
+    # denominations = [10, 5, 2, 1]
     target = 200
-    target = 16
-    ways_to_200 = get_coin_sums(target, denominations)
+    # target = 16
+    count = get_coin_sums_iterative(target, denominations)
 
-    return len(ways_to_200)
+    return count
 
 
 def get_num_coin_sum_quick_dirty():
 
     denominations = [200, 100, 50, 20, 10, 5, 2, 10]
+    denominations = [200, 100, 50, 20, 10, 5, 2, 1]
 
-    target = 100
+    target = 200
     ways_to_200 = [1 # (a, b, c, d, e, f, g, h)
                    for a in range(0, target + 1, denominations[0])
                    for b in range(0, target + 1, denominations[1])
@@ -112,8 +123,10 @@ def get_num_coin_sum_quick_dirty():
 
 def main():
     # answer = get_num_coin_sum()
-    answer = get_num_coin_sum_quick_dirty()
+    answer = get_num_coin_sum_iterative()
     print(f"The Answer to Project Euler 031 is {answer}")
+
+    #  The Answer to Project Euler 031 is 73682
 
 
 if __name__ == "__main__":
