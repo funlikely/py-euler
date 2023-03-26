065524fb024d279e9bc79c23eef0a2c75b15a88b
oldflat <smJU.20tackom$eat>
2023-03-26 01:04:08 -0400

    euler 036 WIP

---
+++ project_euler_036_double-base_palindromes.py
@@ -0,0 +1,28 @@
+"""
+    Double-base palindromes
+    
+    Problem 36
+    
+    The decimal number, 585 = 10010010012 (binary), is palindromic in both bases.
+    
+    Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+    
+    (Please note that the palindromic number, in either base, may not include leading zeros.)
+
+
+"""
+
+
+def get_doubledromes():
+    pass
+
+
+def main():
+    answer = get_doubledromes()
+    print(f"The Answer to Project Euler 036 is {sum(answer)}")
+
+    # The Answer to Project Euler 036
+
+
+if __name__ == "__main__":
+    main()
