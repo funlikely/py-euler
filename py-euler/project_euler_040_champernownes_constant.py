28b1da8fbfa7c204d735593171f1660ffa642ae7
oldflat <smJU.20tackom$eat>
2023-03-31 00:15:19 -0400

    project euler 040 WIP

---
+++ py-euler/project_euler_040_champernownes_constant.py
@@ -0,0 +1,32 @@
+"""
+    Champernowne's constant
+
+    Problem 40
+
+    An irrational decimal fraction is created by concatenating the positive integers:
+
+    0.123456789101112131415161718192021...
+
+    It can be seen that the 12th digit of the fractional part is 1.
+
+    If dn represents the nth digit of the fractional part, find the value of the following expression.
+
+    d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
+"""
+import math
+
+
+def champernowne_challenge():
+    digits = [1, 2, 3]
+    return math.prod(digits)
+
+
+def main():
+    answer = champernowne_challenge()
+    print(f"The Answer to Project Euler 040 is {answer}")
+
+    # The Answer to Project Euler 040
+
+
+if __name__ == "__main__":
+    main()
