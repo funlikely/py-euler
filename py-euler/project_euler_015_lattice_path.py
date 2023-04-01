0dcb1a2be45625bf0b0562a8568f34785d6af06d
oldflat <smJU.20tackom$eat>
2023-03-31 22:20:29 -0400

    euler 14-19 cleanup

--- py-euler/project_euler_015_lattice_path.py
+++ py-euler/project_euler_015_lattice_path.py
@@ -1,16 +1,18 @@
-#
-#
-# Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
-#
-# How many such routes are there through a 20×20 grid?
+"""
+    Lattice paths
+
+    Problem 15
+
+    Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+
+    How many such routes are there through a 20×20 grid?
+
+"""
 
 size = 21
 count_table = []
 for i in range(size):
     count_table.append([[]] * size)
-# for i in range(size):
-#     count_table[i][i] = i
-# print(count_table)
 
 
 def count_paths(m, n):
@@ -28,10 +30,22 @@ def count_paths(m, n):
         return result
 
 
-count_paths(20, 20)
+def get_route_count():
+    answer = count_paths(20, 20)
+    for i in range(size):
+        for j in range(size):
+            print("i = {0}, j = {1}, count paths = {2}".format(str(i), str(j), str(count_table[i][j])))
 
-for i in range(size):
-    for j in range(size):
-        print("i = {0}, j = {1}, count paths = {2}".format(str(i), str(j), str(count_table[i][j])))
+    print(count_table)
+    return answer
 
-print(count_table)
+
+def main():
+    answer = get_route_count()
+    print(f"The Answer to Project Euler 015 is {answer}")
+
+    # The Answer to Project Euler 015 is 137846528820
+
+
+if __name__ == "__main__":
+    main()
--- py-euler/project_euler_016_power_digit_sum.py
+++ py-euler/project_euler_016_power_digit_sum.py
@@ -1,41 +1,36 @@
-# Power digit sum
-#
-# Problem 16
-#
-# 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
-#
-# What is the sum of the digits of the number 2^1000?
+"""
+    Power digit sum
 
-def add_two_string_ints(a, b):
-    carry = 0
-    total = []
-    for i in range(min(len(a), len(b))):
-        digit_sum = int(a[len(a) - i - 1]) + int(b[len(b) - i - 1]) + carry
-        if digit_sum > 9:
-            carry = 1
-            digit_sum -= 10
-        else:
-            carry = 0
-        total.append(str(digit_sum))
-    total.reverse()
-    return ''.join(total)
+    Problem 16
+
+    2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+    What is the sum of the digits of the number 2^1000?
+"""
+from utilities.bignum import *
 
 
-n = '0' * 320 + '1'
+def get_power_digit_sum():
+    n = BigNum('0' * 320 + '1')
 
-for i in range(1000):
-    n = add_two_string_ints(n, n)
-    print('{0} : {1}'.format(str(i + 1).rjust(4, " "), n))
+    for i in range(1000):
+        n = n.add(n)
+        print(f'{str(i + 1).rjust(4, " ")} : {n.value}')
 
-power_digit_sum = 0
-for i in range(len(n)):
-    power_digit_sum += int(n[i])
+    power_digit_sum = 0
+    for i in range(len(n.value)):
+        power_digit_sum += int(n.value[i])
 
-print("power digit sum = {0}".format(str(power_digit_sum)))
-
-
-
-
+    print(f"power digit sum = {power_digit_sum}")
+    return power_digit_sum
 
 
+def main():
+    answer = get_power_digit_sum()
+    print(f"The Answer to Project Euler 016 is {answer}")
 
+    # The Answer to Project Euler 016 is 1366
+
+
+if __name__ == "__main__":
+    main()
