65acde45af7c669d5c08ef2bced39ace44fee2b2
oldflat <smJU.20tackom$eat>
2023-04-01 20:45:06 -0400

    moving all the bignum duplicated code into the bignum class

--- py-euler/project_euler_020_factorial_digit_sum.py
+++ py-euler/project_euler_020_factorial_digit_sum.py
@@ -10,70 +10,24 @@
 
     Find the sum of the digits in the number 100!
 """
-
-import math
-
-
-def add_two_string_ints(a, b):
-    carry = 0
-    total = []
-    max_length = max(len(a), len(b))
-    a = a.rjust(max_length, '0')
-    b = b.rjust(max_length, '0')
-    for i in range(max_length):
-        digit_sum = int(a[len(a) - i - 1]) + int(b[len(b) - i - 1]) + carry
-        if digit_sum > 9:
-            carry = 1
-            digit_sum -= 10
-        else:
-            carry = 0
-        total.append(str(digit_sum))
-    if carry == 1:
-        total.append(str(carry))
-    total.reverse()
-    return ''.join(total)
-
-
-def multiply_two_string_ints(s, t):
-    a = []
-    for i in range(len(s)):
-        a.append(int(s[len(s) - 1 - i]))
-    b = []
-    for i in range(len(t)):
-        b.append(int(t[len(t) - 1 - i]))
-    product_list = [0] * (len(a) + len(b))
-    for i in range(len(a)):
-        for j in range(len(b)):
-            product_list[i + j] += a[i] * b[j]
-    temp_product = 0
-    for i in range(len(product_list)):
-        temp_product += int(product_list[i] * math.pow(10, i))
-    width = len(product_list) + 1
-    sub_product_list = [''] * width
-    for i in range(len(product_list)):
-        sub_product_list[i] = str(product_list[i]).rjust(width - i, '0').ljust(width, '0')
-    result = sub_product_list[0]
-    for i in range(1, len(sub_product_list) - 1):
-        result = add_two_string_ints(result, sub_product_list[i])
-
-    return result.lstrip('0')
+from utilities.bignum import *
 
 
 def get_factorial_digit_sum():
-    print(multiply_two_string_ints('123', '12'))
-    print(multiply_two_string_ints('99', '99'))
-    print(multiply_two_string_ints('99999999999', '99999999999'))
-    print(multiply_two_string_ints('999999999999999', '999999999999999'))
+    print(BigNum('123').multiply(BigNum('12')).value)
+    print(BigNum('99').multiply(BigNum('99')).value)
+    print(BigNum('99999999999').multiply(BigNum('99999999999')).value)
+    print(BigNum('999999999999999').multiply(BigNum('999999999999999')).value)
 
-    factorial = "1"
+    factorial = BigNum('1')
     for i in range(1, 100):
-        factorial = multiply_two_string_ints(factorial, str(i))
+        factorial = factorial.multiply(BigNum(str(i)))
 
-    print(factorial)
+    print(factorial.value)
 
     factorial_digits_total = 0
-    for i in range(len(factorial)):
-        factorial_digits_total += int(factorial[i])
+    for i in range(len(factorial.value)):
+        factorial_digits_total += int(factorial.value[i])
     print("the sum of digits in 100! is " + str(factorial_digits_total))
     return factorial_digits_total
 
