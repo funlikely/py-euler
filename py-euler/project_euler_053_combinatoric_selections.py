3c1d402bdfcedecada4e77e69bf74ac30af6ea49
Oldflat <ducksalt+github@gmail.com>
2023-04-06 17:25:40 -0400

    Project Euler 053 solution

--- py-euler/project_euler_053_combinatoric_selections.py
+++ py-euler/project_euler_053_combinatoric_selections.py
@@ -10,20 +10,23 @@
     How many, not necessarily distinct, values of
     for n-choose-r, are greater than one-million?
 """
+from math import factorial as f
 
 
 def get_combination_count():
     count = 0
-    for i in range(1,101):
-        count += 1
-    return 0
+    for i in range(1, 101):
+        for j in range (1, i):
+            if f(i)/(f(j)*f(i-j)) > 10**6:
+                count += 1
+    return count
 
 
 def main():
     answer = get_combination_count()
     print(f"The Answer to Project Euler 053 is {answer}")
 
-    # The Answer to Project Euler 053 is
+    # The Answer to Project Euler 053 is 4075
 
 
 if __name__ == "__main__":
