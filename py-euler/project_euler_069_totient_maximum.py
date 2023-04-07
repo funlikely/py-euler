aae957cd655f251b97186386a677341707dc241e
oldflat <smJU.20tackom$eat>
2023-04-07 08:38:30 -0400

    Euler 069 WIP II

--- py-euler/project_euler_069_totient_maximum.py
+++ py-euler/project_euler_069_totient_maximum.py
@@ -3,16 +3,11 @@
 
     Problem 69
 
-    Euler's Totient function, $\phi(n)$ [sometimes called the phi function], is defined as the number of positive
-    integers not exceeding $n$ which are relatively prime to $n$. For example, as $1$, $2$, $4$, $5$, $7$, and $8$,
-    are all less than or equal to nine and relatively prime to nine, $\phi(9)=6$. $n$ 	Relatively Prime 	$\phi(n)$
-    	$n/\phi(n)$ 2 	1 	1 	2 3 	1,2 	2 	1.5 4 	1,3 	2 	2 5 	1,2,3,4 	4 	1.25 6 	1,
-    	5 	2 	3 7 	1,2,3,4,5,6 	6 	1.1666... 8 	1,3,5,7 	4 	2 9 	1,2,4,5,7,8 	6 	1.5 10 	1,3,7,
-    	9 	4 	2.5
+    Euler's Totient function, [sometimes called the phi function], is defined as the number of positive integers not
+    exceeding which are relatively prime to . For example, as , , , , , and , are all less than or equal to nine and
+    relatively prime to nine,
 
-    It can be seen that $n = 6$ produces a maximum $n/\phi(n)$ for $n\leq 10$.
-
-    Find the value of $n\leq 1\,000\,000$ for which $n/\phi(n)$ is a maximum.
+    Find the value of n <= 1000000 for which is n/phi(n) a maximum.
 
     NOTE: phi(n) = n * prod(1 - 1/p) for all p primes dividing n
     E.g., phi(26) = 26 * 1/2 * 12/13 = 12
@@ -24,18 +19,29 @@ pp = PrimeProcessor(1000000)
 primes = pp.primes_up_to(1000000)
 
 
+def get_prime_divisors(n):
+    prime_factors = div.prime_factors(n)  # this method is failing because it's not going high enough.  test 5086121 for example
+    return [p for i, p in enumerate(prime_factors[0]) if prime_factors[1][i] > 0]
+
+
 def totient(n):
-    prime_factors = div.prime_factors(n)
+    prime_factors = get_prime_divisors(n)
     prod = 1
-    for p in prime_factors:
+    for p in prime_factors[0]:
         prod *= (1-1/p)
     return n * prod
 
 
 def get_totient_kind_of_maximum():
     int_max = 1000000
-
-    return 0
+    answer = 1
+    tot_qot_max = 1
+    for n in range(2, int_max):
+        if n / totient(n) > tot_qot_max:
+            tot_qot_max = n / totient(n)
+            answer = n
+            print(f"latest max n: {n}, totient: {totient(n)}, quotient: {tot_qot_max}")
+    return answer
 
 
 def main():
