44224431439df17d27f8d3e87c21dd3c499cd077
oldflat <smJU.20tackom$eat>
2023-04-06 23:17:33 -0400

    Euler 062 work in progress

--- py-euler/project_euler_062_cubic_permutations.py
+++ py-euler/project_euler_062_cubic_permutations.py
@@ -8,17 +8,39 @@
     
     Find the smallest cube for which exactly five permutations of its digits are cube.
 """
+import math
 
 
 def find_smallest_special_cube():
-    return 0
+    mag = 2
+    max_mag = 8
+    # cubes = [n**3 for n in range(10**max_mag)]
+    while mag < max_mag:
+        sub_cubes = [i**3 for i in range(int(math.pow(10**mag, 1/3)), int(math.pow(10**(mag+1), 1/3)+1))]
+        print(f"checking from {sub_cubes[0]} to {sub_cubes[-1:][0]}")
+        pizza = {}
+        for sub_cube in sub_cubes:
+            sub_cube_string = ''.join(sorted(str(sub_cube)))
+            if sub_cube_string not in pizza:
+                pizza[sub_cube_string] = 1
+            else:
+                pizza[sub_cube_string] += 1
+        found_results = [val for i, val in enumerate(pizza.values()) if val > 2]
+        if len(found_results) > 0:
+            found_cubes = [sub_cube for sub_cube in sub_cubes if pizza[''.join(sorted(str(sub_cube)))] > 2]
+            print(f"found cubes: {found_cubes}")
+            return min(sorted(found_cubes))
+        mag += 1
+    print(f"failed to find cubes : (")
+    return -1
 
 
 def main():
     answer = find_smallest_special_cube()
     print(f"The Answer to Project Euler 062 is {answer}")
 
-    # The Answer to Project Euler 062 is 
+    # WROOOOONG
+    # The Answer to Project Euler 062 is 41063625
 
 
 if __name__ == "__main__":
