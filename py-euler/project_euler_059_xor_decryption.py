ccd414acbaf101d528ee75dce59d79400d881847
Oldflat <ducksalt+github@gmail.com>
2023-04-08 16:13:40 -0400

    Project Euler 059 V

--- py-euler/project_euler_059_xor_decryption.py
+++ py-euler/project_euler_059_xor_decryption.py
@@ -24,21 +24,48 @@
     and the knowledge that the plain text must contain common English words, decrypt the message and find the sum of 
     the ASCII values in the original text.
 """
+from typing import List
+
+
+def decrypt(data: List[int], key: str) -> List[int]:
+    int_key = [ord(key[i]) - 97 for i in range(3)]
+    decrypt_data = [c ^ int_key[i % 3] for i, c in enumerate(data)]
+    return decrypt_data
 
 
 def get_xor_decryption_information():
     file = open('data/project_euler_069.txt')
     data = file.readline().split(',')
+    data = [int(d) for d in data]
     print(data)
     print(len(data))
     d = [[int(c) for i, c in enumerate(data) if i % 3 == t] for t in range(3)]
     print(d[0])
     print(d[1])
     print(d[2])
-    histographs = [ {c: d[j].count(c) for i, c in enumerate(d[j])} for j in range(3)]
-    print(histographs[0])
-    print(histographs[1])
-    print(histographs[2])
+    histograms = [{c: d[j].count(c) for i, c in enumerate(d[j])} for j in range(3)]
+    print(histograms[0])
+    print(histograms[1])
+    print(histograms[2])
+
+    count = 0
+    for a, b, c in [(chr(a + 97), chr(b + 97), chr(c + 97)) for a in range(26) for b in range(26) for c in range(26)]:
+        key = a + b + c
+        decrypt_data = decrypt(data, key)
+        decrypt_char = [chr(c) for c in decrypt_data]
+
+        def f(acc, x):
+            return acc + 1 if 97 <= x <= 122 or 65 <= x <= 90 else acc
+
+        accumulator = 0
+        [accumulator := f(accumulator, x) for x in decrypt_data]
+
+        if decrypt_char.count('e') > 10 or decrypt_char.count('a') > 10:
+            print(f"key = {key}, decrypted characters = {decrypt_char}")
+
+        if count % 4000 == 0:
+            print(f"key = {key}, decrypted characters = {decrypt_char}")
+        count += 1
     return 0
 
 
