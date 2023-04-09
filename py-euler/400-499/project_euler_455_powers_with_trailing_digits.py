8b796e1ceca6c7e933321eb86aaf15fc2d1ad0f6
Oldflat <ducksalt+github@gmail.com>
2023-04-09 09:39:01 -0400

    Project Euler 455 initial checkin

---
+++ py-euler/400-499/project_euler_455_powers_with_trailing_digits.py
@@ -0,0 +1,34 @@
+"""
+    Powers With Trailing Digits
+
+    Problem 455
+
+    Let f(n) be the largest positive integer x less than 109 such that the last 9 digits of nx form the number x (including leading zeros), or zero if no such integer exists.
+
+    For example:
+
+        f(4) = 411728896 (4411728896 = ...490411728896)
+        f(10) = 0
+        f(157) = 743757 (157743757 = ...567000743757)
+        ∑ f(n), 2 ≤ n ≤ 103 = 442530011399
+
+    Find ∑ f(n), 2 ≤ n ≤ 106.
+"""
+
+def f(n: int) -> int:
+    return 0
+
+
+def get_sum_of_powers() -> int:
+    return 0
+
+
+def main():
+    answer = get_sum_of_powers()
+    print(f"The Answer to Project Euler 455 is {answer}")
+
+    # The Answer to Project Euler 455
+
+
+if __name__ == "__main__":
+    main()
