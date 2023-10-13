ea4e1cc5ecd1455614059122809019dce65a060b
oldflat <smJU.20tackom$eat>
2023-10-13 19:51:52 -0400

    initial checkin, Euler-104

---
+++ py-euler/101_200/project_euler_104_pandigital_fib_ends.py
@@ -0,0 +1,37 @@
+"""
+    Pandigital Fibonacci Ends
+
+    Problem 104
+
+    Let F be the Fibonnacci sequence, F1 = 1, F2 = 1, F3 = 2, .., so that F = 1, 1, 2, 3, 5, . . .
+
+    It turns out that F541 which contains 113 digits, is the first Fibonacci number for which the last nine digits
+    are 1-9 pandigital (contain all the digits 1-9, but not necessarily in order). And F2749, which contains 575
+    digits, is the first Fibonacci number for which the first nine digits are 1-9 pandigital.
+
+    Given that Fk
+    is the first Fibonacci number for which the first nine digits AND the last nine digits are 1-9 pandigital, find k.
+"""
+
+debug = True
+
+
+def get_answer():
+    return 1
+
+
+def debug_and_investigation():
+    pass
+
+
+def main():
+    if debug:
+        debug_and_investigation()
+    answer = get_answer()
+    print(f"The Answer to Project Euler 104 is {answer}")
+
+    # The Answer to Project Euler 104 is __
+
+
+if __name__ == "__main__":
+    main()
