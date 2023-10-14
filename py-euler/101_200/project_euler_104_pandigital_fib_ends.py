3ef7331e30e40723811d7cf6761f6b8370f49cba
oldflat <smJU.20tackom$eat>
2023-10-13 23:02:25 -0400

    WIP iIi, Euler-104

--- py-euler/101_200/project_euler_104_pandigital_fib_ends.py
+++ py-euler/101_200/project_euler_104_pandigital_fib_ends.py
@@ -14,26 +14,52 @@
 """
 from utilities.bignum import *
 
+start_fresh = False
+
+# consider using
+# sys.set_int_max_str_digits(50000)
+# sys.set_int_max_str_digits(4300)
+# sys.get_int_max_str_digits()
+
+
+def initialize_fib_numbers_with_data_file_if_possible():
+    if start_fresh:
+        return 2, BigNum('1'), BigNum('1')
+    f = open("data/project_euler_104.txt")
+    lines = f.readlines()
+    counter = int(lines[-3])
+    fib_a = BigNum(lines[-2].strip('\n'))
+    fib_b = BigNum(lines[-1].strip('\n'))
+    return counter, fib_a, fib_b
+
 
 def get_answer() -> (int, BigNum):
-    fib_a = fib_b = BigNum('1')
-    counter = 2
+    counter, fib_a, fib_b = initialize_fib_numbers_with_data_file_if_possible()
+
     check = True
     first_nine = last_nine = ''
+    first_nine_count = last_nine_count = 0
 
     while check:
         fib_c = fib_a.add(fib_b)
         fib_b = fib_a
         fib_a = fib_c
 
-        if len(str(fib_a.value)) > 18:
+        fib_a_digit_count = len(str(fib_a.value))
+        if fib_a_digit_count > 18:
             first_nine = str(fib_a.value)[:9]
             last_nine = str(fib_a.value)[-9:]
         counter += 1
 
-        if counter % 6000 == 0 and debug:
-            print(f"Fib({counter}): {first_nine}...{last_nine}")
+        if counter % 8000 == 0 and debug:
+            print(f'Fib({counter}): {first_nine}...{last_nine}, {fib_a_digit_count} digits long. {first_nine_count} \
+instances of first nine digits being pandigital, and {last_nine_count} instances of last nine digits being \
+pandigital')
 
+        if set(first_nine) == set('123456789'):
+            first_nine_count += 1
+        if set(last_nine) == set('123456789'):
+            last_nine_count += 1
         if set(first_nine) == set(last_nine) == set('123456789'):
             check = False
 
