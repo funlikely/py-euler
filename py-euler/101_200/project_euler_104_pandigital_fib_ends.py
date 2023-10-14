e358240f8caa83cf7abb921df6ecbd82db128f10
oldflat <smJU.20tackom$eat>
2023-10-13 20:29:26 -0400

    WIP, Euler-104

--- py-euler/101_200/project_euler_104_pandigital_fib_ends.py
+++ py-euler/101_200/project_euler_104_pandigital_fib_ends.py
@@ -3,7 +3,7 @@
 
     Problem 104
 
-    Let F be the Fibonnacci sequence, F1 = 1, F2 = 1, F3 = 2, .., so that F = 1, 1, 2, 3, 5, . . .
+    Let F be the Fibonacci sequence, F1 = 1, F2 = 1, F3 = 2, .., so that F = 1, 1, 2, 3, 5, . . .
 
     It turns out that F541 which contains 113 digits, is the first Fibonacci number for which the last nine digits
     are 1-9 pandigital (contain all the digits 1-9, but not necessarily in order). And F2749, which contains 575
@@ -12,12 +12,35 @@
     Given that Fk
     is the first Fibonacci number for which the first nine digits AND the last nine digits are 1-9 pandigital, find k.
 """
+from utilities.bignum import *
+
+
+def get_answer() -> (int, BigNum):
+    fib_a = fib_b = BigNum('1')
+    counter = 2
+    check = True
+    first_nine = last_nine = ''
+
+    while check:
+        fib_c = fib_a.add(fib_b)
+        fib_b = fib_a
+        fib_a = fib_c
+
+        if len(str(fib_a.value)) > 18:
+            first_nine = str(fib_a.value)[:9]
+            last_nine = str(fib_a.value)[-9:]
+        counter += 1
+
+        if (counter % 1567 == 66 or counter % 2323 == 123) and debug:
+            print(f"Fib({counter}): {first_nine}...{last_nine}")
+
+        if set(first_nine) == set(last_nine) == set('123456789'):
+            check = False
+
+    return counter, fib_a.value
+
 
 debug = True
-
-
-def get_answer():
-    return 1
 
 
 def debug_and_investigation():
@@ -27,8 +50,10 @@ def debug_and_investigation():
 def main():
     if debug:
         debug_and_investigation()
-    answer = get_answer()
+    answer, fib_number = get_answer()
     print(f"The Answer to Project Euler 104 is {answer}")
+    if debug:
+        print(f"The fibonacci number starts with {fib_number.value[:30]} and is {len(fib_number.value)} digits long.")
 
     # The Answer to Project Euler 104 is __
 
