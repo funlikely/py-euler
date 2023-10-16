c26e100390f178ffcc9293f0ae36b4c43c615e5a
oldflat <smJU.20tackom$eat>
2023-10-16 19:06:58 -0400

    initial commit, Euler-060

---
+++ py-euler/project_euler_060_prime_pair_sets.py
@@ -0,0 +1,26 @@
+"""
+    Prime Pair Sets
+
+    Problem 60
+
+    The primes $3$, $7$, $109$, and $673$, are quite remarkable. By taking any two primes and concatenating them in
+    any order the result will always be prime. For example, taking $7$ and $109$, both $7109$ and $1097$ are prime.
+    The sum of these four primes, $792$, represents the lowest sum for a set of four primes with this property.</p>
+    <p>Find the lowest sum for a set of five primes for which any two primes concatenate to produce another prime.
+"""
+from typing import List
+
+
+def get_answer():
+    return 1
+
+
+def main():
+    answer = get_answer()
+    print(f"The Answer to Project Euler 060 is {answer}")
+
+    # The Answer to Project Euler 060 is
+
+
+if __name__ == "__main__":
+    main()
