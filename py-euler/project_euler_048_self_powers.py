49a80bc7329689c0ef6b9b17d2d3ce058f1c5fe5
oldflat <smJU.20tackom$eat>
2023-10-16 13:53:34 -0400

    Solution, Euler-048

--- py-euler/project_euler_048_self_powers.py
+++ py-euler/project_euler_048_self_powers.py
@@ -7,21 +7,24 @@
 
     Find the last ten digits of the series, 11 + 22 + 33 + ... + 10001000.
 """
-import utilities.bignum as bg
-
 
 def get_big_prime_digits():
-    a = bg.BigNum(1)
-    for i in range(2, 200):
-        a = bg.BigNum(1)
-    return 0
+    sequence = [0] * 1001
+    for i in range(1, 1001):
+        accumulator = 1
+        for j in range(1, i+1):
+            accumulator *= i
+            accumulator %= 10**11
+        sequence[i] = accumulator
+    answer = str(sum(sequence))[-10:]
+    return answer
 
 
 def main():
     answer = get_big_prime_digits()
     print(f"The Answer to Project Euler 048 is {answer}")
 
-    # The Answer to Project Euler 048
+    # The Answer to Project Euler 048 is 9110846700
 
 
 if __name__ == "__main__":
