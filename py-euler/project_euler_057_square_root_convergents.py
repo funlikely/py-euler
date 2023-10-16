1a40aa4428e31e79996f309e00f11b7c50a73c75
oldflat <smJU.20tackom$eat>
2023-10-16 14:02:11 -0400

    Initial info, Euler-057

---
+++ py-euler/project_euler_057_square_root_convergents.py
@@ -0,0 +1,34 @@
+"""
+    Square Root Convergents
+    Problem 57
+
+    sqrt(2) = 1 + 1/(2 + 1/(2 + 1/(2 + ...)))
+
+    Fractions in the partial series are
+
+    3/2, 7/5, 17/12, 41/29, 99/70, 239/169, 577/408, 1393/985, ...
+
+    1393/985 is the first example where the number of digits in the numerator exceeds the number of digits in the
+    denoninator.
+
+    In the first one-thousand expansions, how many fractions contain a numerator with more digits than the denominator?
+"""
+
+
+debug = True
+
+
+def get_answer():
+    return 1
+
+
+def main():
+
+    answer = get_answer()
+    print(f"The Answer to Project Euler 057 is {answer}")
+
+    # The Answer to Project Euler 057
+
+
+if __name__ == "__main__":
+    main()
