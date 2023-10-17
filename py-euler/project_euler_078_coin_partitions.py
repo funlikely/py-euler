7379b6aca54a761dd2f58f5b13df60f6947c2123
oldflat <smJU.20tackom$eat>
2023-10-17 14:32:44 -0400

    initial, Euler-078

---
+++ py-euler/project_euler_078_coin_partitions.py
@@ -0,0 +1,36 @@
+"""
+    Coin Partitions
+
+
+    Problem 78
+
+    Let p(n) represent the number of different ways in which n coins can be separated into piles. For example,
+    five coins can be separated into piles in exactly seven different ways, so p(5) = 7
+
+    OOOOO
+    OOOO   O
+    OOO   OO
+    OOO   O   O
+    OO   OO   O
+    OO   O   O   O
+    O   O   O   O   O
+
+    Find the least value of n
+    for which p(n) is divisible by one million.
+
+"""
+
+
+def get_answer():
+    return 1
+
+
+def main():
+    answer = get_answer()
+    print(f"The Answer to Project Euler 078 is {answer}")
+
+    # The Answer to Project Euler 078 is
+
+
+if __name__ == "__main__":
+    main()
