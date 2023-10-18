7e976514ecc4b5d8223fdeef8ee850c17610ab1c
Oldflat <ducksalt+github@gmail.com>
2023-10-18 19:21:43 -0400

    some investigation, definitions, Project Euler 078

--- py-euler/project_euler_078_coin_partitions.py
+++ py-euler/project_euler_078_coin_partitions.py
@@ -18,6 +18,36 @@
     Find the least value of n
     for which p(n) is divisible by one million.
 
+    Let's define p(n) = P(n,n) where P(n,m) is the number of ways to partition n coins using a maximum
+    partition size of m.
+
+    Specially define P(n,0) = 1
+
+    Some examples:
+    P(n,0) = 1
+    P(n,1) = 1
+
+    P(n,m) = sum(P(i, min(i, n-i))) for i from 1 to n
+
+    p(7) = P(7,7)
+    =  P(0,0)
+     + P(1,1)
+     + P(2,2)
+     + P(3,3)
+     + P(4,3) + P(1,1)
+     + P(5,2) + P(3,2) + P(1,1)
+     + P(6,1)
+     + P(7,0)
+
+    P(4,3)
+    =  xxx x, xx xx, xx x x, x x x x
+    =  P(1,1)
+     + P(4,2)
+     + P(4,1)
+
+    P(5,2)
+    =  xx xx x, xx x x x, x x x x x
+    = P(1,1) + P(3,1) + P(1,1)
 """
 
 
