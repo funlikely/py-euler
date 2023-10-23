db4e42b57ca88179f7a848ad901fee1a0bb7d9a4
oldflat <smJU.20tackom$eat>
2023-10-23 16:05:51 -0400

    initial commit, Euler-063

---
+++ py-euler/project_euler_063_powerful_digit_counts.py
@@ -0,0 +1,24 @@
+"""
+    Powerful Digit Counts
+    
+    Problem 63
+    
+    The $5$-digit number, $16807=7^5$, is also a fifth power. Similarly, the $9$-digit number, $134217728=8^9$, is a ninth power.</p>
+<p>How many $n$-digit positive integers exist which are also an $n$th power?
+"""
+import math
+
+
+def get_answer():
+    pass
+
+
+def main():
+    answer = get_answer()
+    print(f"The Answer to Project Euler 063 is {answer}")
+
+    # The Answer to Project Euler 063 is ___
+
+
+if __name__ == "__main__":
+    main()
