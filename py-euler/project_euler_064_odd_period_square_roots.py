9c8471e5320c89e735bb7a66cd002cf242eb5362
oldflat <smJU.20tackom$eat>
2023-10-26 21:49:34 -0400

    initial prob description, Euler-064

---
+++ py-euler/project_euler_064_odd_period_square_roots.py
@@ -0,0 +1,41 @@
+"""
+    Odd Period Square Roots
+
+    Problem 64
+    
+    All square roots are periodic when written as continued fractions and can be written in the form:
+
+    $\displaystyle \quad \quad \sqrt{N}=a_0+\frac 1 {a_1+\frac 1 {a_2+ \frac 1 {a3+ \dots}}}$
+
+    The first ten continued fraction representations of (irrational) square roots are:
+    
+    $\quad \quad \sqrt{2}=[1;(2)]$, period=$1$
+    $\quad \quad \sqrt{3}=[1;(1,2)]$, period=$2$
+    $\quad \quad \sqrt{5}=[2;(4)]$, period=$1$
+    $\quad \quad \sqrt{6}=[2;(2,4)]$, period=$2$
+    $\quad \quad \sqrt{7}=[2;(1,1,1,4)]$, period=$4$
+    $\quad \quad \sqrt{8}=[2;(1,4)]$, period=$2$
+    $\quad \quad \sqrt{10}=[3;(6)]$, period=$1$
+    $\quad \quad \sqrt{11}=[3;(3,6)]$, period=$2$
+    $\quad \quad \sqrt{12}=[3;(2,6)]$, period=$2$
+    $\quad \quad \sqrt{13}=[3;(1,1,1,1,6)]$, period=$5$
+    
+    Exactly four continued fractions, for $N \le 13$, have an odd period.
+    How many continued fractions for $N \le 10\,000$ have an odd period?
+"""
+import math
+
+
+def get_answer():
+    pass
+
+
+def main():
+    answer = get_answer()
+    print(f"The Answer to Project Euler 064 is {answer}")
+
+    # The Answer to Project Euler 064
+
+
+if __name__ == "__main__":
+    main()
