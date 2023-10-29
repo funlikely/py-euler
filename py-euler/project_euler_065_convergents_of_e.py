245702afb79f22920e82f6c7b83b9f33ebc789df
oldflat <smJU.20tackom$eat>
2023-10-29 13:51:12 -0400

    initial problem description, Euler-065

---
+++ py-euler/project_euler_065_convergents_of_e.py
@@ -0,0 +1,37 @@
+"""
+    Convergents of e
+
+    Problem 65
+
+    Continued fraction of e:
+    [2; 1, 2, 1, 1, 4, 1, 1, 6, 1, ..., 1, 2k, 1, ...]
+
+    Find the sum of digits in the numerator of the 100th convergent of the continued fraction of e
+
+"""
+import math
+
+
+debug = True
+
+
+def get_answer():
+    return 1
+
+
+def investigate():
+    return
+
+
+def main():
+    if debug:
+        investigate()
+
+    answer = get_answer()
+    print(f"The Answer to Project Euler 065 is {answer}")
+
+    # The Answer to Project Euler 065 is ___
+
+
+if __name__ == "__main__":
+    main()
