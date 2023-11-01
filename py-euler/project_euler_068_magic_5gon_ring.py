3dc507934dc806c1e94685e5070f7cc199072879
oldflat <smJU.20tackom$eat>
2023-11-01 00:16:34 -0400

    wip i, Euler-068

--- py-euler/project_euler_068_magic_5gon_ring.py
+++ py-euler/project_euler_068_magic_5gon_ring.py
@@ -11,18 +11,14 @@ from typing import List
 
 
 def is_magic(gon):
-    if sum(gon[0:3])\
-            == sum(gon[3:6])\
-            == sum(gon[6:9])\
-            == sum(gon[9:12])\
-            == sum(gon[12:15]):
+    if sum(gon[0:3]) == sum(gon[3:6]) == sum(gon[6:9]) == sum(gon[9:12]) == sum(gon[12:15]):
         return True
     return False
 
 
 def is_valid(gon):
     if gon[2] == gon[4] and gon[5] == gon[7] and gon[8] == gon[10] and gon[11] == gon[13] and gon[14] == gon[1]:
-        if set(gon) == {1,2,3,4,5,6,7,8,9,10}:
+        if set(gon) == {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}:
             return True
     return False
 
@@ -32,29 +28,47 @@ def get_best_5gon_string():
     print(f'example {example} is magic: {is_magic(example)}')
     print(f'example {example} is valid: {is_valid(example)}')
 
-    for a in range(1,11):
-        for b in range(1,11):
-            for c in range(1,11):
-                for d in range(1,11):
-                    for e in range(1,11):
-                        for f in range(1,11):
-                            for g in range(1,11):
-                                for h in range(1,11):
-                                    for i in range(1,11):
-                                        gon = [a, b, c, d, c, e, f, e, g, h, g, i, b]
-                                        if is_magic(gon):
-                                            print(f'{gon} is magic')
+    counter = 0
+    for a in range(1, 11):
+        for b in range(1, 11):
+            if a == b:
+                break
+            for c in range(1, 11):
+                if a == c or b == c:
+                    break
+                for d in range(1, 11):
+                    if a == d or b == d or c == d:
+                        break
+                    for e in range(1, 11):
+                        if a == e or b == e or c == e or d == e:
+                            break
+                        for f in range(1, 11):
+                            if a == f or b == f or c == f or d == f or e == f:
+                                break
+                            for g in range(1, 11):
+                                if a == g or b == g or c == g or d == g or e == g or f == g:
+                                    break
+                                for h in range(1, 11):
+                                    for i in range(1, 11):
+                                        for j in range(1, 11):
+                                            gon = [a, b, c, d, c, e, f, e, g, h, g, i, j, i, b]
+                                            counter += 1
+                                            if counter % 1000000 == 0:
+                                                print(f'testing {gon}')
+                                            if not is_valid(gon):
+                                                continue
+                                            if is_magic(gon):
+                                                print(f'{gon} is magic')
 
     return 1
 
 
 def main():
-
     answer = get_best_5gon_string()
     print(f"The Answer to Project Euler 068 is {answer}")
 
     # The Answer to Project Euler 068 is
-    
+
 
 if __name__ == "__main__":
     main()
