d7787c8e423e93a8bcdfc8f809aef009bb1319dc
oldflat <smJU.20tackom$eat>
2023-10-31 23:47:50 -0400

    initial commit, Euler-068

---
+++ py-euler/project_euler_068_magic_5gon_ring.py
@@ -0,0 +1,26 @@
+"""
+    Magic 5-gon Ring
+
+    Problem 68
+
+    Using the numbers 1 to 10, and depending on arrangements, it is possible to form 16- and 17-digit strings. What
+    is the maximum 16-digit string for a "magic" 5-gon ring?
+"""
+import math
+from typing import List
+
+
+def get_best_5gon_string():
+    return 1
+
+
+def main():
+
+    answer = get_best_5gon_string()
+    print(f"The Answer to Project Euler 068 is {answer}")
+
+    # The Answer to Project Euler 068 is
+    
+
+if __name__ == "__main__":
+    main()
