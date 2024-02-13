3d172a6245bba056f183c3a8ba737b835d9cde2d
funlikely <smJU.20tackom$eat>
2024-02-12 22:52:30 -0500

    initial look, Euler-070

---
+++ py-euler/project_euler_070_Totient_Permutation.py
@@ -0,0 +1,27 @@
+"""
+    Totient Permutation
+    Problem 70
+
+    Find the value of n, 1 < n < 10^7, for which phi(n) is a permutation of n,
+     and the ratio n / phi(n) produces a minimum
+
+"""
+import math
+import fractions
+
+debug = True
+
+
+def get_answer():
+    return 0
+
+
+def main():
+    answer = get_answer()
+    print(f"The Answer to Project Euler 070 is {answer}")
+
+    # The Answer to Project Euler 070 is
+
+
+if __name__ == "__main__":
+    main()
